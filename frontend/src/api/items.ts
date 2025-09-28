5ddd068ddc114fdaf9bc5aafaf8e8d7c397e344e
infinotiver <88645086+infinotiver@users.noreply.github.com>
2025-09-28 16:23:26 +0530

    feat:  add API func to retrieve item by item_id

--- frontend/src/api/items.ts
+++ frontend/src/api/items.ts
@@ -1,12 +1,18 @@
 import apiClient from "./apiClient";
 export interface Item {
-  id: string;
+  _id: string;
   sink_id: string;
   content: string;
   type: string;
 }
+
 export async function getUserItems(userId: string) {
   const res = await apiClient.get(`/items/user/?auth_user_id=${userId}`);
   console.log(res.data);
+  return res.data;
+}
+
+export async function getItem(itemId: string) {
+  const res = await apiClient.get(`/items/item/${itemId}`);
   return res.data;
 }
