9ff6b404bf715722b60e0d56e191e65a1f16a2c5
infinotiver <88645086+infinotiver@users.noreply.github.com>
2025-09-28 16:26:08 +0530

    feat: integrate item retrieval with react-query and update ItemViewPage layout

--- frontend/src/app/ItemViewPage.tsx
+++ frontend/src/app/ItemViewPage.tsx
@@ -1,12 +1,26 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getItem } from "@/api/items";
 import ImagePreview from "@/components/itemview/ImagePreview";
 import ItemDetails from "@/components/itemview/ItemDetails";
+import GalleryGrid from "@/components/masonry/galleryGrid";
 
 function ItemViewPage() {
   const { itemID } = useParams();
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState("");
+
+  const {
+    data: itemData,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["items", itemID],
+    queryFn: () =>
+      itemID ? getItem(itemID) : Promise.reject("Item not found"),
+    enabled: !!itemID,
+  });
 
   const handleAddTag = (tag: string) => {
     if (tag && !selectedTags.includes(tag)) {
@@ -18,18 +32,25 @@ function ItemViewPage() {
     setSelectedTags(selectedTags.filter((t) => t !== tag));
   };
 
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return `<p>Failed to load item. ${error}</p>`;
+
   return (
-    <div className="flex gap-6">
-      <div className="max-w-96 h-auto aspect-auto">
-        {itemID ? <ImagePreview itemID={itemID} /> : <p>Item not found</p>}
+    <div className="flex gap-6 w-full h-full">
+      <div className="flex flex-col gap-6 w-3/5">
+        {itemData ? <ImagePreview item={itemData} /> : <p>Item not found</p>}
+        <ItemDetails
+          item={itemData}
+          selectedTags={selectedTags}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          handleAddTag={handleAddTag}
+          handleDeleteTag={handleDeleteTag}
+        />
       </div>
-      <ItemDetails
-        selectedTags={selectedTags}
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-        handleAddTag={handleAddTag}
-        handleDeleteTag={handleDeleteTag}
-      />
+      <div className="w-2/5">
+        <GalleryGrid columns={2}/>
+      </div>
     </div>
   );
 }
