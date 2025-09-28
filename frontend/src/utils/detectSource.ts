de2a73537ea8ebe6ce8362e18ec74e9e927c5e28
infinotiver <88645086+infinotiver@users.noreply.github.com>
2025-09-28 16:42:27 +0530

    feat: add more sources and icons in detectSource

--- frontend/src/utils/detectSource.ts
+++ frontend/src/utils/detectSource.ts
@@ -1,18 +1,24 @@
 import React from "react";
-import { FiLink } from "react-icons/fi";
-import { FaPinterest } from "react-icons/fa";
+import { FiLink, FiLinkedin } from "react-icons/fi";
+import { FaAmazon, FaDiscord, FaEtsy, FaFacebook, FaGithub, FaInstagram, FaPinterest, FaReddit, FaTwitter, FaUnsplash, FaYoutube } from "react-icons/fa";
 import type { JSX } from "react/jsx-runtime";
 
 const sourceMap: Record<string, { icon: JSX.Element; name: string }> = {
   "pinterest.com": { icon: React.createElement(FaPinterest), name: "Pinterest" },
   "i.pinimg.com": { icon: React.createElement(FaPinterest), name: "Pinterest" },
-  "youtube.com": { icon: React.createElement(FiLink), name: "YouTube" },
-  "youtu.be": { icon: React.createElement(FiLink), name: "YouTube" },
-  "instagram.com": { icon: React.createElement(FiLink), name: "Instagram" },
-  "twitter.com": { icon: React.createElement(FiLink), name: "Twitter" },
-  "facebook.com": { icon: React.createElement(FiLink), name: "Facebook" },
-  "amazon.com": { icon: React.createElement(FiLink), name: "Amazon" },
-  "etsy.com": { icon: React.createElement(FiLink), name: "Etsy" },
+  "youtube.com": { icon: React.createElement(FaYoutube), name: "YouTube" },
+  "youtu.be": { icon: React.createElement(FaYoutube), name: "YouTube" },
+  "instagram.com": { icon: React.createElement(FaInstagram), name: "Instagram" },
+  "twitter.com": { icon: React.createElement(FaTwitter), name: "Twitter" },
+  "facebook.com": { icon: React.createElement(FaFacebook), name: "Facebook" },
+  "amazon.com": { icon: React.createElement(FaAmazon), name: "Amazon" },
+  "etsy.com": { icon: React.createElement(FaEtsy), name: "Etsy" },
+  "unsplash.com": { icon: React.createElement(FaUnsplash), name: "Unsplash" },
+  "discord.com": { icon: React.createElement(FaDiscord), name: "Discord" },
+  "discord.gg": { icon: React.createElement(FaDiscord), name: "Discord" },
+  "github.com": { icon: React.createElement(FaGithub), name: "GitHub" },
+  "linkedin.com": { icon: React.createElement(FiLinkedin), name: "LinkedIn" },
+  "reddit.com": { icon: React.createElement(FaReddit), name: "Reddit" },
 };
 
 export const detectSource = (
