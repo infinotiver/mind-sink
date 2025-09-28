7e446e443742034c63f719360829f93614b151ff
infinotiver <88645086+infinotiver@users.noreply.github.com>
2025-09-28 16:26:29 +0530

    feat: integrate with API queries

--- frontend/src/components/itemview/ImagePreview.tsx
+++ frontend/src/components/itemview/ImagePreview.tsx
@@ -1,21 +1,28 @@
 import { Button } from "@/components/ui/button";
-import { FiDownload, FiShare2 } from "react-icons/fi";
+import type { Item } from "@/api/items";
+import { FiDownload, FiShare2, FiInfo } from "react-icons/fi";
 
-function ImagePreview({ itemID }: { itemID: string }) {
+function ImagePreview({ item }: { item: Item }) {
   return (
-    <div className="relative flex justify-center items-start">
+    <div className="relative flex justify-center items-center">
       <img
-        src={`/pins/${itemID}.png`}
+        src={item.content}
         alt="Preview"
-        className="rounded-lg shadow-lg object-contain max-h-100 max-w-100 aspect-auto"
+        className="rounded-lg shadow-lg object-contain max-h-[60vh] max-w-full"
       />
-      <div className="absolute top-4 right-4 flex gap-2">
-        <Button size="icon" variant="secondary">
+      <div className="absolute top-4 right-4 flex gap-2 bg-accent/75 p-2 rounded-2xl">
+        <Button size="icon" variant="default">
           <FiDownload size={18} />
         </Button>
-        <Button size="icon" variant="secondary">
+        <Button size="icon" variant="default">
           <FiShare2 size={18} />
         </Button>
+      </div>
+      <div className="absolute bottom-4 left-4">
+        <div className="flex items-center gap-1 text-sm text-foreground bg-accent/75 p-2 rounded-2xl">
+          <FiInfo />
+          {item._id}
+        </div>
       </div>
     </div>
   );
