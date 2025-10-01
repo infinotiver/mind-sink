d686d7670ff9ff4f32532cf227dc54d7775ee5a5
infinotiver <88645086+infinotiver@users.noreply.github.com>
2025-10-01 11:18:04 +0530

    refactor: enhance layout and styling in HomePage and ManifestoPage components

--- frontend/src/app/ManifestoPage.tsx
+++ frontend/src/app/ManifestoPage.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/common/navbar";
 export default function ManifestoPage() {
   return (
-    <div>
+    <div className="p-4 sm:p-8">
       <Navbar />
-      <div className="flex flex-col items-center justify-center min-h-screen text-center gap-y-4 px-4 sm:px-8">
-        <h1 className="text-[min(8vw,84px)] font-medium mt-6 sm:mt-8 mb-3 sm:mb-4 flex flex-wrap items-center justify-center gap-3 md:gap-4">Mind Sink Manifesto</h1>
-        <p className="text-xl font-semibold mb-6">
-          <strong>Let your thoughts sink.</strong>
+      <div className="flex flex-col gap-y-4 p-4 sm:px-8 bg-accent/20 backdrop-blur-md rounded-lg">
+        <h1 className="text-[min(8vw,84px)] font-medium mt-4 sm:mt-6 mb-3 sm:mb-4 flex flex-wrap items-center justify-start gap-3 md:gap-4">
+          Mind Sink Manifesto
+        </h1>
+        <p className="text-2xl font-medium mb-6">
+          Let your thoughts <span className="font-lora italic">sink</span>.
         </p>
         <p className="text-lg mb-4">
           Mind Sink is built for clarity, not noise. No endless scroll, no ads,
@@ -19,7 +21,7 @@ export default function ManifestoPage() {
           collect and revisit what matters.
         </p>
         <h2 className="text-2xl font-semibold mb-2">What We Stand For</h2>
-        <ul className="list-disc list-inside mb-4">
+        <ul className="list-disc list-inside mb-4 text-left">
           <li>Focus: No infinite feeds or clutter.</li>
           <li>Simplicity: Clean and minimal by design.</li>
           <li>Control: Your thoughts stay yours.</li>
