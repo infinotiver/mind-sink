38649dc85dbad2fd71fa37785cc29e7a9ce459c3
infinotiver <88645086+infinotiver@users.noreply.github.com>
2025-10-20 19:36:05 +0530

    style: improve layout and formatting in ProfilePage component

--- frontend/src/app/ProfilePage.tsx
+++ frontend/src/app/ProfilePage.tsx
@@ -1,10 +1,11 @@
-import { useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { getUserProfile } from "@/api/profile";
-import { FiShare } from "react-icons/fi";
-import { Button } from "@/components/ui/button";
-import Loading from "@/components/ui/loading";
-import ErrorAlert from "@/components/ui/error-alert";
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getUserProfile } from '@/api/profile';
+import { FiShare } from 'react-icons/fi';
+import { Button } from '@/components/ui/button';
+import Loading from '@/components/ui/loading';
+import ErrorAlert from '@/components/ui/error-alert';
+import { Separator } from '@/components/ui/separator';
 
 export default function ProfilePage() {
   const { userID } = useParams<{ userID: string }>();
@@ -14,9 +15,8 @@ export default function ProfilePage() {
     isError,
     error,
   } = useQuery({
-    queryKey: ["userProfile", userID],
-    queryFn: () =>
-      userID ? getUserProfile(userID) : Promise.reject("User ID is missing"),
+    queryKey: ['userProfile', userID],
+    queryFn: () => (userID ? getUserProfile(userID) : Promise.reject('User ID is missing')),
     enabled: !!userID,
   });
 
@@ -30,27 +30,26 @@ export default function ProfilePage() {
       />
     );
   return (
-    <div className="flex flex-col items-center p-6">
+    <div className="flex flex-col items-center p-6 gap-y-2">
       <div className="w-32 h-32 bg-muted rounded-lg flex items-center justify-center">
         <span className="text-muted-foreground">
-          <img src={profile.avatar_url} alt="No Profile Pic" />
+          <img src={profile.avatar_url} alt="No Profile Pic" className="rounded-4xl" />
         </span>
       </div>
       {/* <div> */}
-      <h1 className="text-xl font-bold mt-4 text-primary">
+      <h1 className="text-2xl font-semibold mt-4 text-primary italic font-lora">
         {profile.username}
       </h1>
-      <a
-        href="#"
-        className="text-secondary-foreground hover:underline text-md"
-      ></a>
-      <div className="flex">
-        <div className="text-gray-400">@</div>
+      <a href="#" className="text-secondary-foreground hover:underline text-md"></a>
+      <div className="flex text-sm gap-2 text-gray-400">
+        {'@'}
         {profile.user_id}
+        <Separator orientation="vertical" />
+        <div className="text-foreground">
+          Joined {new Date(profile.created_at).toLocaleDateString()}
+        </div>
       </div>
-      <div className="flex items-center gap-4 text-muted-foreground text-sm m-2">
-        Joined {new Date(profile.created_at).toLocaleDateString()}
-      </div>
+
       <Button variant="secondary">
         <FiShare />
         Share Account
