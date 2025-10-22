7ce7713924077ef0617b2a3fd3835c79e4c77a0a
infinotiver <88645086+infinotiver@users.noreply.github.com>
2025-10-22 17:23:42 +0530

    refactor: ui update on login page

--- frontend/src/app/LoginPage.tsx
+++ frontend/src/app/LoginPage.tsx
@@ -1,17 +1,29 @@
-import Navbar from "@/components/common/navbar";
-import { Button } from "@/components/ui/button";
-import { FaDiscord } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import Navbar from '@/components/common/navbar';
+import { Button } from '@/components/ui/button';
+import { FaDiscord } from 'react-icons/fa';
+import { TbBrandGoogle, TbBrandGithub } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
+import { Separator } from '@/components/ui/separator';
 
 export default function LoginPage() {
   return (
     <>
       <Navbar />
-      <div className="h-screen flex flex-col items-center justify-center">
+      <div className="h-screen flex flex-col items-center justify-center gap-4 px-4 ">
+        <div className="w-full max-w-xs text-center">
+          <div className="rounded-2xl border-2 bg-accent/25 backdrop-blur-3xl shadow-md py-6 px-5">
+            <div className="flex items-center justify-center gap-3">
+              <h1 className="text-2xl md:text-3xl font-semibold tracking-tight">
+                Login or Create an Account
+              </h1>
+            </div>
+            
+          </div>
+        </div>
         <Button
-          variant="outline"
+          variant="default"
           size="lg"
-          className="flex items-center gap-2"
+          className="flex items-center gap-2 w-full max-w-xs"
           asChild
         >
           <Link to={`${import.meta.env.VITE_API_URL}/auth/login`}>
@@ -19,6 +31,32 @@ export default function LoginPage() {
             Login with Discord
           </Link>
         </Button>
+
+        <div className="w-full max-w-xs">
+          <Separator className="my-2" />
+        </div>
+
+        <div className="flex flex-col gap-2 w-full max-w-xs">
+          <Button
+            variant="outline"
+            size="lg"
+            className="w-full flex items-center justify-center gap-3"
+            disabled
+          >
+            <TbBrandGoogle />
+            Login with Google
+          </Button>
+
+          <Button
+            variant="outline"
+            size="lg"
+            className="w-full flex items-center justify-center gap-3"
+            disabled
+          >
+            <TbBrandGithub />
+            Login withGitHub
+          </Button>
+        </div>
       </div>
     </>
   );
